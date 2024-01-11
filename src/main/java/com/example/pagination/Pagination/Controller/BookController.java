597b8e76a0f14330fd3775295eaed0b0b99a8c7c
aadithya-ds <aadithyaprasad.p@datasirpi.com>
2024-01-11 17:10:13 +0530

    Added Logger

--- src/main/java/com/example/pagination/Pagination/Controller/BookController.java
+++ src/main/java/com/example/pagination/Pagination/Controller/BookController.java
@@ -3,6 +3,7 @@ package com.example.pagination.Pagination.Controller;
 
 import com.example.pagination.Pagination.Entity.Book;
 import com.example.pagination.Pagination.Services.BookServices;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +13,7 @@ import java.util.List;
 @RestController
 @CrossOrigin
 @RequestMapping("/api/bookstore")
+@Slf4j
 public class BookController {
 
 
@@ -22,5 +24,16 @@ public class BookController {
     @GetMapping
     public Page<Book> getBooks(@RequestParam(defaultValue = "0") int page,@RequestParam(defaultValue = "10") int size){
         return bookServices.getallbooks(page,size);
+    }
+
+    @GetMapping("/logs")
+    public String getLog(){
+        log.trace("This is a TRACE message");
+        log.debug("This is a DEBUG message");
+        log.info("This is an INFO message");
+        log.warn("This is a WARN message");
+        log.error("This is an ERROR message");
+
+        return "Logger Exicuted";
     }
 }
