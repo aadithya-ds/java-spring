dce838f5042103db192999d3220fa75dfe2d830a
aadithya-ds <aadithyaprasad.p@datasirpi.com>
2024-01-25 13:12:39 +0530

    Logger Added

--- src/main/java/com/example/validator/controller/BookController.java
+++ src/main/java/com/example/validator/controller/BookController.java
@@ -8,6 +8,8 @@ import com.example.validator.repository.BookRepository;
 import com.example.validator.service.BookService;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -30,6 +32,8 @@ public class BookController {
     private BookRepository bookRepository;
 
 
+    private static final Logger logger = LoggerFactory.getLogger(BookController.class);
+
 
     @PostMapping("/")
     public String addBook( @Valid @RequestBody BookDto bookData) throws GlobalExceptionHandler {
@@ -39,7 +43,7 @@ public class BookController {
 
     @GetMapping
     public ResponseEntity<List<Book>> getAllBooks(){
-
+        logger.info("Fetching all books");
         return new ResponseEntity<>(bookService.getAllBooks(),HttpStatus.OK);
     }
 
@@ -48,8 +52,10 @@ public class BookController {
         Book book = bookService.getById(id);
 
         if (book != null) {
+            logger.info("Fetching book with id: {}", id);
             return new ResponseEntity<>(book, HttpStatus.OK);
         } else {
+            logger.warn("Book with id {} not found", id);
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
