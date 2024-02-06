7b86f51d6f0a9b718e9e1e092c395fd44b5aaa11
PRAKSH-DATASIRPI <prakash.m@datasirpi.com>
2024-02-06 11:15:55 +0530

    done

---
+++ src/main/java/com/auth/springsecurity/repository/UserInfoRepository.java
@@ -0,0 +1,15 @@
+package com.auth.springsecurity.repository;
+
+import com.auth.springsecurity.entity.UserInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserInfoRepository extends JpaRepository<UserInfo,Integer> {
+
+    Optional<UserInfo> findByName(String userName);
+
+
+}
